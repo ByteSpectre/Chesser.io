e8512170c75fffcbd10e543a4a1ef063ad583e5e
ByteSpectre <magomedovmagush@gmail.com>
2024-06-11 12:56:12 +0300

    reducer - context

--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -1,50 +1,51 @@
 import './Pieces.css'
 import Piece from './Piece'
-import { useRef , useState } from 'react'
-import { copyPosition, createPosition } from '../../helper'
+import { useRef  } from 'react'
+import { useAppContext }from '../../contexts/Context'
 
+import { copyPosition} from '../../helper'
+import {  makeNewMove } from '../../reducer/actions/move'
 const Pieces = () => {
+
+    const { appState , dispatch } = useAppContext();
+    const currentPosition = appState.position[appState.position.length-1]
+
     const ref = useRef()
-
-
-    const [state,setState] = useState(createPosition())
 
     const calculateCoords = e => {
         const {top,left,width} = ref.current.getBoundingClientRect()
         const size = width / 8
         const y = Math.floor((e.clientX - left) / size) 
         const x = 7 - Math.floor((e.clientY - top) / size)
-
         return {x,y}
     }
 
-
     const onDrop = e => {
         e.preventDefault()
-        const newPosition = copyPosition (state)
+        const newPosition = copyPosition (currentPosition)
         const {x,y} = calculateCoords(e)
 
         const [p,rank,file] = e.dataTransfer.getData("text").split(',')
         newPosition[Number(rank)][Number(file)] = ''
         newPosition[x][y] = p
-        setState (newPosition)
+        dispatch(makeNewMove({newPosition}))
+
     }
 
     const onDragOver = e => {e.preventDefault()}
-
     return <div 
         className='pieces' 
         ref={ref} 
         onDrop={onDrop} 
         onDragOver={onDragOver} > 
-        {state.map((r,rank) => 
+        {currentPosition.map((r,rank) => 
             r.map((f,file) => 
-                state[rank][file]
+                currentPosition[rank][file]
                 ?   <Piece 
                         key={rank+'-'+file} 
                         rank = {rank}
                         file = {file}
-                        piece = {state[rank][file]}
+                        piece = {currentPosition[rank][file]}
                     />
                 :   null
             )   
