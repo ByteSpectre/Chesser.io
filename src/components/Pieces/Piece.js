2a654f7bcdadd7cafc2f8b3ae2531bf8c8c8319c
ByteSpectre <magomedovmagush@gmail.com>
2024-06-11 12:48:27 +0300

    dragging functionality

--- src/components/Pieces/Piece.js
+++ src/components/Pieces/Piece.js
@@ -3,13 +3,24 @@ const Piece = ({
     file,
     piece,
 }) => {
-
+    const onDragStart = e => {
+        e.dataTransfer.effectAllowed = "move"
+        e.dataTransfer.setData("text/plain",`${piece},${rank},${file}`)
+        setTimeout(() => {
+            e.target.style.display = "none"
+        }, 0)
+    }
+    const onDragEnd = e => {
+        e.target.style.display = 'block'
+    }
 
 
     return (
         <div 
             className={`piece ${piece} p-${file}${rank}`}
-            draggable={true}   
+            draggable={true}
+            onDragStart={onDragStart} 
+            onDragEnd={onDragEnd}  
         />)
 }
 
